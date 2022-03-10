f4ec4421dfb39764a3f5bda12bd363a8f7748802
Gonzalo Juarez <gonzalojuarezte@gmail.com>
2022-03-10 22:35:17 +0100

    .software.list and add bugs on Main.c

--- src/Main.c
+++ src/Main.c
@@ -1,5 +1,16 @@
 #include "Main.h"
 
-void setup() {}
+#include <stddef.h>
 
+double *mean(int *arr, size_t length) {
+  long long sum = 0;
+
+  for (size_t i = 0; i < length; ++i)
+    sum += arr[i];
+
+  double mean = (double)sum / length;
+  return &mean;
+}
+
+void setup() {}
 void loop() {}
