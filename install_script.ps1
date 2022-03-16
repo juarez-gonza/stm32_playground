064a4bbebcc1bc25590d1315a9d0d2215fd98d77
juarez-gonza <gonzalojuarezte@gmail.com>
2022-03-16 02:37:44 -0700

    remove one argument from chocoInstallIfNotFound in install_scripts.ps1

--- install_script.ps1
+++ install_script.ps1
@@ -1,24 +1,23 @@
 function chocoInstallIfNotFound() {
 	param(
 		[String]$programName,
-		[String]$packageName,
-		[String]$chocoParameters
+		[String]$packageName
 	)
 
 	if ((Get-Command $programName -ErrorAction SilentlyContinue) -eq $null) {
 		Write-Host "$programName was not found in your path... installing cmake with chocolatey"
-		choco install $packageName $chocoParameters
+		choco install $packageName -y
 		refreshenv
 		Get-Command $programName
 	} else {
-			Write-Host "$programName found. Skipping installation"
+		Write-Host "$programName found. Skipping installation"
 	}
 }
 
 chocoInstallIfNotFound "git.exe" "git.install"
-chocoInstallIfNotFound "python" "python3" ""
-chocoInstallIfNotFound "gcc.exe" "mingw" ""
-chocoInstallIfNotFound "arm-none-eabi-gcc.exe" "gcc-arm-embedded" ""
-chocoInstallIfNotFound "make.exe" "make" ""
-chocoInstallIfNotFound "cmake.exe" "cmake" "--version=3.22.1 -y --force --installargs 'ADD_CMAKE_TO_PATH=System'"
-chocoInstallIfNotFound "clangd.exe" "llvm" ""
+chocoInstallIfNotFound "python" "python3"
+chocoInstallIfNotFound "gcc.exe" "mingw"
+chocoInstallIfNotFound "arm-none-eabi-gcc.exe" "gcc-arm-embedded"
+chocoInstallIfNotFound "make.exe" "make"
+chocoInstallIfNotFound "cmake.exe" "cmake"
+chocoInstallIfNotFound "clangd.exe" "llvm"
